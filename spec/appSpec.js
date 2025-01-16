dc2802be16c76f7568b1f238faa83ff2106e5511
daniiii2 <122824902+daniiii2@users.noreply.github.com>
2025-01-16 17:04:33 +0100

    add : convertSingleMinutesRow case 0

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,4 +1,11 @@
-import { Main } from "../src/app";
-describe("Main class should implments a berlin clock ", function() {
+import { Main } from "../src/app.js";
+describe("Main class should implments a berlin clock ", function(){
     const main = new Main();
+
+    it("given 0 should return '0000' ", function() {
+        const result = main.convertSimpleMinutesRow(0);
+
+        expect(result).toBe("0000");
+    });
+    
 });
--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
 export class Main{
+
+    convertSimpleMinutesRow(minutes){
+        return "0000";
+    }
     
 }
