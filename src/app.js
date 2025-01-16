1f278ad866b5cd852c04456e690a950d294acfc7
daniiii2 <122824902+daniiii2@users.noreply.github.com>
2025-01-16 17:24:42 +0100

    refactor : convertSingleMinutesRow in a minimlist function

--- src/app.js
+++ src/app.js
@@ -1,10 +1,5 @@
 export class Main {
     convertSimpleMinutesRow(minutes) {
-        const singleMinutes = minutes % 5;
-        if (singleMinutes === 0) return "0000";
-        if (singleMinutes === 1) return "Y000";
-        if (singleMinutes === 2) return "YY00";
-        if (singleMinutes === 3) return "YYY0";
-        return "YYYY";
+        return "Y".repeat(minutes % 5).padEnd(4, "0");
     }
 }
